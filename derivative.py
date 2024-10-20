5f494972d7dd2a668607997f31dc95a05a588e9c
barkrawc <bartlomiejkrawczyk15@gmail.com>
2024-10-20 12:13:11 +0200

    Czyszczenie w detect_maxima

--- derivative.py
+++ derivative.py
@@ -7,22 +7,6 @@ def detect_maxima(x, t, plot=False):
     maxima_x = x[maxima_indices]
     maxima_y = t[maxima_indices]
 
-    # Filtruj maksima tak, aby pierwsze zawsze było brane, a kolejne tylko, gdy różnica czasu wynosi co najmniej 200 ms
-    filtered_maxima_indices = [maxima_indices[0]]  # Zawsze bierzemy pierwsze maksimum
-    last_max_time = t[maxima_indices[0]]  # Czas pierwszego maksimum
-
-    for index in maxima_indices[1:]:
-        current_time = t[index]
-        if current_time - last_max_time >= 0.2:
-            filtered_maxima_indices.append(index)
-            last_max_time = current_time
-
-    filtered_maxima_indices = np.array(filtered_maxima_indices)
-    
-    # Zwróć punkty x oraz y odpowiadające przefiltrowanym maksimom
-    maxima_x_r = x[filtered_maxima_indices]
-    maxima_y_r = t[filtered_maxima_indices]
-
     if plot:
         # Wykres oryginalnego sygnału
         plt.figure()
@@ -38,7 +22,7 @@ def detect_maxima(x, t, plot=False):
         # Wykres z przefiltrowanymi maksimami
         plt.figure()
         plt.plot(t, x, label='Sygnał EKG', color='b')
-        plt.scatter(maxima_y_r, maxima_x_r, color='g', label='Przefiltrowane maksima', zorder=5)
+        plt.scatter(maxima_y, maxima_x, color='g', label='Przefiltrowane maksima', zorder=5)
         plt.title("Wykryte maksima po filtracji (co najmniej 200 ms)")
         plt.xlabel("Czas [s]")
         plt.ylabel("Napięcie [V]")
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from filters import low_filter_design, high_filter_design
 from differ import differential, integral, power
 from derivative import detect_maxima
 
-file_path = '../data/data/100_MLII.dat'
-# file_path="C:/Users/bartl/Downloads/data/data/100_MLII.dat"
+#file_path = '../data/data/100_MLII.dat'
+file_path="C:/Users/bartl/Downloads/data/data/100_MLII.dat"
 ecg = pd.read_csv(file_path, delimiter='\t').values.ravel()
 ecg = ecg[2000:20000]
             
@@ -15,7 +15,6 @@ f_down = 5
 f_up = 15
 f_p = 360
 t = np.arange(0, len(ecg) / f_p, 1 / f_p)
-# print(t[1:5])
 l_filter = low_filter_design(len(ecg)/5, f_up, f_p, False)
 h_filter = high_filter_design(len(ecg)/5, f_down, f_p, False)
 ecg_filtered = np.convolve(l_filter, ecg, 'same')
