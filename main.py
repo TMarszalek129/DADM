21507ac746fb68849387708cf7db34f90b5d0454
Tomek <tomek12912@gmail.com>
2024-10-22 11:38:48 +0200

    Changing structure of alghoritm architecture

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from filters import low_filter_design, high_filter_design
 from differ import differential, integral, power
 from derivative import detect_maxima
 
-#file_path = '../data/data/100_MLII.dat'
-file_path="C:/Users/bartl/Downloads/data/data/100_MLII.dat"
+file_path = '../data/data/100_MLII.dat'
+# file_path="C:/Users/bartl/Downloads/data/data/100_MLII.dat"
 ecg = pd.read_csv(file_path, delimiter='\t').values.ravel()
-ecg = ecg[2000:20000]
+# ecg = ecg[2000:20000]
             
 f_down = 5
 f_up = 15
@@ -29,46 +29,44 @@ SPKI = 0.0001
 NPKI = 0.000001
 t1 = NPKI + 0.25 * (SPKI - NPKI)
 t2 = 0.5 * t1
-thr = [t1, t2]
 s_peaks, n_peaks = [], []
 s_times, n_times = [], []
 
-for ind in (0, 1):
-    t_actual = thr[ind]
-    for i in range(len(values)):
-
-        if values[i] > t_actual:
-            if (times[i] - times[i-1]) < refr:
-                if values[i] > values[i-1]:
-                    if times[i-1] in s_times:
-                        s_times.pop()
-                        s_peaks.pop()
-                        if times[i-1] not in n_times:
-                            n_times.append(times[i-1])
-                            n_peaks.append(values[i-1])
-                            NPKI = 0.125 * values[i] + 0.875 * NPKI
-                    if times[i] not in s_times:
-                        s_peaks.append(values[i])
-                        s_times.append(times[i])
-                        SPKI = 0.125 * values[i] + 0.875 * SPKI
-                else:
-                    if times[i] not in n_times:
-                        n_peaks.append(values[i])
-                        n_times.append(times[i])
-                        NPKI = 0.125 * values[i] + 0.875 * NPKI
-            else:
-                if times[i] not in s_times:
-                    s_peaks.append(values[i])
-                    s_times.append(times[i])
-                    SPKI = 0.125 * values[i] + 0.875 * SPKI
-        else:
-            if times[i] not in n_times:
-                n_peaks.append(values[i])
-                n_times.append(times[i])
-                NPKI = 0.125 * values[i] + 0.875 * NPKI
-
-        thr[0] = NPKI + 0.25 * (SPKI - NPKI)
-        thr[1] = 0.5 * t1
+for i in range(len(values)):
+    if not values[i] > t1 and times[i] not in n_times:
+        n_peaks.append(values[i])
+        n_times.append(times[i])
+        NPKI = 0.125 * values[i] + 0.875 * NPKI
+        t1 = NPKI + 0.25 * (SPKI - NPKI)
+        t2 = 0.5 * t1
+        continue
+    if not (times[i] - times[i-1]) < refr and times[i] not in s_times:
+        s_peaks.append(values[i])
+        s_times.append(times[i])
+        SPKI = 0.125 * values[i] + 0.875 * SPKI
+        t1 = NPKI + 0.25 * (SPKI - NPKI)
+        t2 = 0.5 * t1
+        continue
+    if not values[i] > values[i-1] and times[i] not in n_times:
+        n_peaks.append(values[i])
+        n_times.append(times[i])
+        NPKI = 0.125 * values[i] + 0.875 * NPKI
+        t1 = NPKI + 0.25 * (SPKI - NPKI)
+        t2 = 0.5 * t1
+        continue
+    if times[i-1] in s_times:
+        s_times.pop()
+        s_peaks.pop()
+        if times[i-1] not in n_times:
+            n_times.append(times[i-1])
+            n_peaks.append(values[i-1])
+            NPKI = 0.125 * values[i] + 0.875 * NPKI
+    if times[i] not in s_times:
+        s_peaks.append(values[i])
+        s_times.append(times[i])
+        SPKI = 0.125 * values[i] + 0.875 * SPKI
+    t1 = NPKI + 0.25 * (SPKI - NPKI)
+    t2 = 0.5 * t1
 
 plt.figure()
 plt.title("EKG")
