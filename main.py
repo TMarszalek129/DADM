18898aa2311598deaafe183e72931c0812645eb5
barkrawc <bartlomiejkrawczyk15@gmail.com>
2024-11-03 17:43:39 +0100

    Sygnał rzeczywisty

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
+import pandas as pd
 
+from perio import interpol, per, per_ls, bandpower
+"""
+# SINUS
 samples = 128
 f_0 = 0.1
 
 t = np.concatenate(([0], np.cumsum(np.random.rand(samples-1))))
 y = np.sin(2 * np.pi * f_0 * t)
 
-interp = interp1d(t, y, kind='linear')
-t_new = np.linspace(0, t[-1], samples)
-y_new = interp(t_new)
+t_new, y_new, f = interpol(t, y, samples)
 
-f = np.linspace(0, 1/(t_new[1] - t_new[0]), samples)
-p = [1/samples * ( (np.sum(y_new * np.cos(2 * np.pi * f[i] * t_new)))**2 + ((np.sum(y_new * np.sin(2 * np.pi * f[i] * t_new)))**2) )  for i in range(samples)]
+p = per(t_new, y_new, f, samples)
 
 t_new_ls = t_new[1:]
 y_ls = y[1:]
-tau = [1 / (4 * np.pi * f[i]) * np.atan(np.sum(np.sin(4 * np.pi * f[i] * t_new_ls)) / np.sum(np.cos(4 * np.pi * f[i] * t_new_ls))) for i in range(1, samples)]
-cos_part = np.array([np.sum(y_ls * np.cos(2 * np.pi * f[i] * (t_new_ls - tau)))**2 / np.sum(np.cos(2 * np.pi * f[i] * (t_new_ls - tau))**2) for i in range(1, samples)])
-sin_part = np.array([np.sum(y_ls * np.sin(2 * np.pi * f[i] * (t_new_ls - tau)))**2 / np.sum(np.sin(2 * np.pi * f[i] * (t_new_ls - tau))**2) for i in range(1, samples)])
-p_ls = 1/2 * (cos_part + sin_part)
 
+p_ls = per_ls(t_new_ls, y_ls, f, samples)
 
 plt.figure()
 plt.plot(t, y)
@@ -37,4 +34,82 @@ plt.show()
 plt.figure()
 plt.plot(f[1:], p_ls)
 plt.title("Periodogram Lomb-Scargle")
+plt.show()
+"""
+
+# RZECZYWISTY
+
+file_path="C:/Users/bartl/Downloads/chf206.dat"
+ecg = pd.read_csv(file_path, delimiter='\t').values.ravel()
+ecg = ecg[2000:4000]
+
+sampl = len(ecg)
+f_s = 360
+
+t_r = np.concatenate(([0], np.cumsum(ecg)))
+y_r = np.sin(2 * np.pi * f_s * t_r)
+
+t_new_r, y_new_r, f_r = interpol(t_r, y_r, sampl)
+
+p_r = per(t_new_r, y_new_r, f_r, sampl)
+
+t_new_ls_r = t_new_r[1:]
+print(len(t_new_ls_r))
+y_ls_r = y_r[2:]
+print(len(y_ls_r))
+
+p_ls_r = per_ls(t_new_ls_r, y_ls_r, f_r, sampl)
+
+plt.figure()
+plt.plot(t_r, y_r)
+plt.plot(t_new_r, y_new_r)
+plt.title("Signal")
+plt.show()
+
+plt.figure()
+plt.plot(f_r, p_r)
+plt.title("Periodogram")
+plt.show()
+
+plt.figure()
+plt.plot(f_r[1:], p_ls_r)
+plt.title("Periodogram Lomb-Scargle")
+plt.show()
+
+
+# Definicja zakresów częstotliwości dla miar HRV
+hf_range = (0.15, 0.4)
+lf_range = (0.04, 0.15)
+vlf_range = (0.0033, 0.04)
+ulf_range = (0, 0.0033)
+tp_range = (0, 0.4)
+
+hf_power = bandpower(f_r[1:], p_ls_r, hf_range)
+lf_power = bandpower(f_r[1:], p_ls_r, lf_range)
+vlf_power = bandpower(f_r[1:], p_ls_r, vlf_range)
+ulf_power = bandpower(f_r[1:], p_ls_r, ulf_range)
+tp_power = bandpower(f_r[1:], p_ls_r, tp_range)
+
+lf_hf_ratio = lf_power / hf_power if hf_power != 0 else np.inf
+
+# Wyświetlanie wyników
+print("HF Power:", hf_power)
+print("LF Power:", lf_power)
+print("VLF Power:", vlf_power)
+print("ULF Power:", ulf_power)
+print("Total Power (TP):", tp_power)
+print("LF/HF Ratio:", lf_hf_ratio)
+
+# Rysowanie widma mocy
+plt.figure(figsize=(10, 6))
+plt.plot(f_r[1:], p_ls_r)
+plt.axvspan(hf_range[0], hf_range[1], color='red', alpha=0.3, label='HF')
+plt.axvspan(lf_range[0], lf_range[1], color='blue', alpha=0.3, label='LF')
+plt.axvspan(vlf_range[0], vlf_range[1], color='green', alpha=0.3, label='VLF')
+plt.axvspan(ulf_range[0], ulf_range[1], color='purple', alpha=0.3, label='ULF')
+plt.title("Widmo mocy HRV")
+plt.xlabel("Częstotliwość [Hz]")
+plt.ylabel("Moc")
+plt.legend()
+plt.grid()
 plt.show()
---
+++ perio.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from scipy.integrate import simps
+
+def interpol(t,y,samples):
+    interp = interp1d(t, y, kind='linear')
+    t_new = np.linspace(0, t[-1], samples)
+    y_new = interp(t_new)
+    f = np.linspace(0, 1/(t_new[1] - t_new[0]), samples)
+    return t_new, y_new, f
+
+def per(t_new, y_new, f, samples):
+    p = [1/samples * ( (np.sum(y_new * np.cos(2 * np.pi * f[i] * t_new)))**2 + ((np.sum(y_new * np.sin(2 * np.pi * f[i] * t_new)))**2) )  for i in range(samples)]
+    return p
+
+def per_ls(t_new_ls, y_ls, f, samples):
+    tau = [1 / (4 * np.pi * f[i]) * np.arctan(np.sum(np.sin(4 * np.pi * f[i] * t_new_ls)) / np.sum(np.cos(4 * np.pi * f[i] * t_new_ls))) for i in range(1, samples)]
+    cos_part = np.array([np.sum(y_ls * np.cos(2 * np.pi * f[i] * (t_new_ls - tau)))**2 / np.sum(np.cos(2 * np.pi * f[i] * (t_new_ls - tau))**2) for i in range(1, samples)])
+    sin_part = np.array([np.sum(y_ls * np.sin(2 * np.pi * f[i] * (t_new_ls - tau)))**2 / np.sum(np.sin(2 * np.pi * f[i] * (t_new_ls - tau))**2) for i in range(1, samples)])
+    p_ls = 1/2 * (cos_part + sin_part)
+    return p_ls
+
+
+def bandpower(frequencies, power_spectrum, freq_range):
+    idx_band = np.logical_and(frequencies >= freq_range[0], frequencies <= freq_range[1])
+    band_power = simps(power_spectrum[idx_band], frequencies[idx_band])
+    return band_power
