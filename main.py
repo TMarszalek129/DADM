22783f218d7a14a4942b8e9dbf0b6154ec2158f1
Tomek <tomek12912@gmail.com>
2024-11-16 13:20:38 +0100

    Bug fix

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 file_path = "../../chf206.dat"
 hrv = pd.read_csv(file_path, delimiter='\t').values.ravel()
 hrv = hrv
-
-hrv_mean_out = hrv - np.mean(hrv)
+mean_hrv = np.mean(hrv)
+hrv_mean_out = np.cumsum(hrv - mean_hrv)
 time = np.cumsum(hrv)
 
 hrv_intervals = []
@@ -15,11 +15,11 @@ interval = []
 LEN = 4
 counter = 0
 
-for i in range(0, len(hrv_mean_out), 4):
-    hrv_intervals.append(hrv_mean_out[i:i+4])
-    time_intervals.append(time[i:i+4])
+for i in range(0, len(hrv_mean_out), LEN):
+    hrv_intervals.append(hrv_mean_out[i:i+LEN])
+    time_intervals.append(time[i:i+LEN])
 
-if(len(hrv_intervals[-1]) != 4):
+if(len(hrv_intervals[-1]) != LEN):
     hrv_intervals.pop(len(hrv_intervals) - 1)
     time_intervals.pop(len(time_intervals) - 1)
 
@@ -34,14 +34,25 @@ for i in range(len(hrv_intervals)):
     yn = X @ np.array(out)
     results.append(yn)
     betas.append(out)
+results = np.array(results)
 
 plt.figure()
 plt.plot(time, hrv_mean_out)
+# plt.ylim(-0.5, 0.5)
+plt.grid()
+plt.xlabel('Time[s]')
+plt.ylabel('Integrated HRV signal [s]')
+plt.title('Integrated HRV')
 plt.show()
 
 plt.figure()
-plt.plot(time_intervals[1], hrv_intervals[1], "r*")
-plt.plot(time_intervals[1], results[1])
+plt.plot(time_intervals, hrv_intervals, "r*")
+for i in range(len(time_intervals)):
+    plt.plot(time_intervals[i], results[i])
 plt.xlim([0, 10])
-plt.ylim([-0.1, 0.1])
+plt.ylim([-0.5, 0.5])
+plt.grid()
+plt.xlabel('$x_{t}$[s]')
+plt.ylabel('y[s]')
+plt.title('n=' + str(LEN))
 plt.show()
