5f97051eed9676898b5a69f73a05ff2f6e1a1548
Tomek <tomek12912@gmail.com>
2024-11-20 10:04:29 +0100

    Bug fix in fluctuation mean squared

--- fluctuation.py
+++ fluctuation.py
@@ -4,7 +4,7 @@ def fluct_mean(y, y_n):
 
     for j in range(len(y)):
         N = len(y[j][0]) * len(y[j][1])
-        F.append(np.sqrt(1/N) * np.sum((y[j] - y_n[j]))**2)
+        F.append(np.sqrt(1/N) * np.sum((y[j] - y_n[j])**2))
 
     F = np.array(F)
 
