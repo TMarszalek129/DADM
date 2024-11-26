6dac331635efbaf66276fab473852e590b6bd73a
barkrawc <bartlomiejkrawczyk15@gmail.com>
2024-11-26 23:36:07 +0100

    Fukcja RANSAC dla ENG

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
-from model import ransac, ols
-
+from model import ransac, ols, ransac_eng, ols_eng, detect_extrema
+"""
 #path = '../../data1.mat'
 path = 'C:/Users/bartl/Downloads/data1.mat'
 
@@ -17,7 +17,18 @@ samples = np.random.choice(indices, 2)
 x_values = data[0, samples]
 y_values = data[1, samples]
 
-
+print(x_values, y_values)
 ransac(x_values, y_values, x, y, data, True)
-ols(x, y, data, True)
+ols(x, y, True)
+"""
+path_eng = 'C:/Users/bartl/Downloads/eng_signals.mat'
 
+data_eng = loadmat(path_eng)
+data_eng = data_eng['eng_signal1']
+
+f_p = 100
+t = np.arange(0, len(data_eng) / f_p, 1 / f_p)
+
+max, min = detect_extrema(data_eng.ravel(), t, True)
+ransac_eng(max[:-1], min[1:], t, data_eng, True)
+#ols_eng(max[:-1], min[1:], t, data_eng, False)
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
 
 def ransac(x_values, y_values, x, y, data, wave):
 
@@ -43,7 +45,7 @@ def ransac(x_values, y_values, x, y, data, wave):
         plt.grid()
         plt.show()
 
-def ols(x, y, data, wave):
+def ols(x, y, wave):
 
     X = np.vstack((x, np.ones(len(x)))).T
     beta, _, _, _ = np.linalg.lstsq(X, y, rcond=None)
@@ -58,3 +60,81 @@ def ols(x, y, data, wave):
         plt.legend()
         plt.grid()
         plt.show()
+
+
+def ransac_eng(max, min, x, y, wave):
+    results = []
+    beta = []
+    l_max = []
+    l_min = []
+    indi = []
+
+    for j in range(len(max)):
+        X_eng = np.array([np.ones(len(x[max[j]:min[j]])), x[max[j]:min[j]]]).T
+        Y_eng = y[max[j]:min[j]]
+
+        betas_eng = np.linalg.inv(X_eng.T @ X_eng) @ X_eng.T @ Y_eng
+        yn = X_eng @ betas_eng
+
+        results.append(yn)
+        beta.append(betas_eng)
+
+        thr = 0.2
+        y_min = yn - thr * np.mean(yn)
+        y_max = yn + thr * np.mean(yn)
+
+        l_max.append(y_max)
+        l_min.append(y_min)
+
+        indi.append(x[max[j]:min[j]])
+
+    if wave:
+        plt.figure()
+        plt.plot(x, y, 'r')
+        for i in range(len(results)):
+            plt.plot(indi[i], l_min[i], 'b-')
+            plt.plot(indi[i], l_max[i], 'b-')
+            plt.plot(indi[i], results[i], 'k-')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Dopasowanie prostej metodą RANSAC')
+        plt.grid()
+        plt.show()
+
+def ols_eng(x, y, wave):
+
+    X = np.vstack((x, np.ones(len(x)))).T
+    beta, _, _, _ = np.linalg.lstsq(X, y, rcond=None)
+    slope, intercept = beta
+
+    if wave:
+        plt.scatter(x, y, label='Dane')
+        plt.plot(x, slope * x + intercept, color='red', label='Dopasowana prosta')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Dopasowanie prostej metodą OLS')
+        plt.legend()
+        plt.grid()
+        plt.show()
+
+def detect_extrema(signal, time, plot_flag):
+    # Wykrywanie maksimów
+    maxima, _ = find_peaks(signal, height=5, distance=80)
+    
+    # Wykrywanie minimów (przez odwrócenie sygnału)
+    minima, _ = find_peaks(-signal, height=5, distance=95)
+    
+    # Jeśli plot_flag jest True, wyświetl wykres
+    if plot_flag:
+        plt.figure(figsize=(10, 6))
+        plt.plot(time, signal, label='Sygnał', color='blue')  # Rysowanie sygnału
+        plt.scatter(time[maxima], signal[maxima], color='red', label='Maksima', zorder=5)  # Rysowanie maksimów
+        plt.scatter(time[minima], signal[minima], color='green', label='Minima', zorder=5)  # Rysowanie minimów
+        plt.title('Wykrywanie maksimów i minimów sygnału')
+        plt.xlabel('Czas')
+        plt.ylabel('Wartość sygnału')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    return maxima, minima
