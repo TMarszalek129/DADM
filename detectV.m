c3d0c86d1de2c9285ab728d925977ee228c60784
Tomek <tomek12912@gmail.com>
2024-12-15 15:51:28 +0100

    Detection of V wave using changible range values

--- detectV.m
+++ detectV.m
@@ -1,46 +1,60 @@
-function [swa, sto, t_max, t_min] = detectV(signal)
+function [swa, sto, t_max, t_min] = detectV(signal, db)
 %DETECTV Summary of this function goes here
 %   This function detects ABR signal's fifth wave
 
-persistent iter
-
-if(isempty(iter))
-    iter = 1;
-else
-    iter = iter + 1;
-end
+persistent min_range
+persistent max_range
 
 [swa,swd] = swt(signal,6,'bior5.5');
 
 swa = swa(6, :);
-
+sto = 0;
+t_min = 0;
+t_max = 0;
 
 fig_proc = figure;
 plot(swa)
 title('SWA after preprocessing')
 
 
-[max, idx_max] = findpeaks(swa);
-localmins = islocalmin(swa);
+[max, idx_max] = findpeaks(swa)
+localmins = islocalmin(swa, 'FlatSelection','first');
 min = swa(localmins);
 idx_min = find(localmins == 1);
 
-min_range = 550 + int16(iter/5) * 10;
-max_range = 700 + int16(iter/5) * 10;
+if(isempty(min_range) & isempty(max_range))
+    min_range = 550;
+    max_range = 700;
+end
+idx_V = [];
 
-idx_V = find(idx_max > min_range & idx_max < max_range);
+while isempty(idx_V)
+    sprintf("Min value of range: %f", min_range)
+    sprintf("Max value of range: %f", max_range)
+    idx_V = find(idx_max > min_range & idx_max < max_range)
+    min_range = min_range + 10;
+    max_range = max_range + 10;
+end
+
+
+
 V_max = max(idx_V(1));
 t_max = idx_max(idx_V(1));
 
 idx_V_min = find(idx_min > idx_max(idx_V(1)));
-idx_V_min = idx_V_min(1);
-V_min = min(idx_V_min);
-t_min = idx_min(idx_V_min);
+try
+    idx_V_min = idx_V_min(1);
+    V_min = min(idx_V_min);
+    t_min = idx_min(idx_V_min);
+   
 
-V_pp = V_max - V_min;
-V_mean = abs(mean(swa));
+    V_pp = V_max - V_min;
+    V_mean = abs(mean(swa));
 
-sto = V_pp / V_mean;
+    sto = V_pp / V_mean;
+catch
+    warning("%f sound is inaudible", db)
+end
 
 close(fig_proc)
 end
--- main.m
+++ main.m
@@ -1,3 +1,5 @@
+clear, clc
+clear detectV
 path_2 = "abr_signal2.mat";
 path_3 = "abr_signal3.mat";
 path_4 = "abr_signal4.mat";
@@ -11,12 +13,12 @@ data3 = abr3.abr_signal3;
 data4 = abr4.abr_signal4;
 
 fp = 100000;
-
+DATA_NUM = data2; % data2 or data3 or data4
 %% Process loop:
 
-for i = 1:length(data2)
-    db = data2{i}.dB;
-    signal = data2{i}.data;
+for i = 1:length(DATA_NUM)
+    db = DATA_NUM{i}.dB;
+    signal = DATA_NUM{i}.data;
 
     ones_arr = transpose(signal(length(signal)-1)*ones(1,24));
     signal = [signal; ones_arr];
@@ -24,15 +26,17 @@ for i = 1:length(data2)
     t = 0:1/fp:(length(signal)-1)*(1/fp);
     t = t * 1000;
 
-    [swa, sto, t_max, t_min] = detectV(signal);
-
-    sprintf("V_pp/V_mean for %f Hz = %f", db, sto)
-
-    figure
-    plot(swa)
-    hold on
-    plot(t_max, swa(t_max), 'r*')
-    plot(t_min, swa(t_min), 'b*')
-    title(['SWA ', num2str(i)])
+    [swa, sto, t_max, t_min] = detectV(signal, db);
+    
+    if(sto)
+        sprintf("V_pp/V_mean for %f Hz = %f", db, sto)
+    
+        figure
+        plot(swa)
+        hold on
+        plot(t_max, swa(t_max), 'r*')
+        plot(t_min, swa(t_min), 'b*')
+        title(['SWA ', num2str(db), ' Hz'])
+    end
 end
 
